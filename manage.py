c88cee278d9c3ffd20e8b7efc7000921b94dabc8
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-26 04:47:33 +0300

    create management packages

---
+++ manage.py
@@ -0,0 +1,18 @@
+from app import create_app,db
+from flask_script import Server,Manager
+from app.models import User
+from flask_migrate import Migrate,MigrateCommand
+
+
+
+app=create_app('development')
+manager=Manager(app)
+manager.add_command('server',Server)
+migrate= Migrate(app,db)
+manager.add_command('migrate',MigrateCommand)
+@manager.shell
+def show_shell_context():
+    return dict(app=app,db=db,User=User)
+
+if __name__=='__main__':
+    manager.run()
