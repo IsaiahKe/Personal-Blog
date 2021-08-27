38fcf0b01a6e7f0d9e12c014d2ca3841c43745eb
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-27 04:49:58 +0300

    updated file

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Server,Manager
-from app.models import User
+from app.models import User,Post,Comment
 from flask_migrate import Migrate,MigrateCommand
 
 
@@ -12,7 +12,7 @@ migrate= Migrate(app,db)
 manager.add_command('migrate',MigrateCommand)
 @manager.shell
 def show_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app = app,db = db,User = User,Post = Post,Comment = Comment)
 
 if __name__=='__main__':
     manager.run()
