2e34d78aa94b506bfc89622f4361dac9ef9f412a
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-27 06:06:55 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
 class Config:
     SECRET_KEY= os.environ.get('SECRET_KEY')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:Access@localhost/blog'
     DEBUG=True
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 class TestConfig(Config):
     pass
 config_options={
