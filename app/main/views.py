ccb3b49febe72c594e6ba03bede604ea7734e7c4
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-27 05:00:18 +0300

    add main view file

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app.requests import get_quote
+from . import main
+from ..models import Comment,Post
+from flask_login import current_user
+from sqlalchemy import desc,select
+# from flask_login import current_user,login_required
+
+@main.route('/')
+def index():
+    
+    title="home"
+    quote=get_quote()
+    posts= Post.query.all()
+    
+    return render_template('index.html',title=title,quote=quote,posts=posts)
