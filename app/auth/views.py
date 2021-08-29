611d7c618e1e3fe14e9c243b25d97d25080b1d9a
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-29 10:21:27 +0300

    add views functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
-from flask import render_template, redirect, url_for,flash,request
-from flask_login import login_user, login_required,logout_user,current_user
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, login_required, logout_user, current_user
 from . import auth
-
-from .forms import LoginForm, RegistrationForm,PostForm,CommentForm,SubscriberForm
-from ..models import User,Post,Comment,Subscribe
+from .forms import LoginForm, PostUpdate, RegistrationForm, PostForm, CommentForm, SubscriberForm, TitleUpdate
+from ..models import User, Post, Comment, Subscribe
 from .. import db
+from ..mail import sendmail
 
 
 @auth.route('/login', methods=['POST', 'GET'])
@@ -31,20 +31,14 @@ def register():
                     email=register.email.data,
                     phone=register.phone.data,
                     password=register.password.data)
-       
 
         db.session.add(user)
         db.session.commit()
+        sendmail("User Sign Up", 'mail/welcome_user', user.email, user=user)
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html', register=register, title=title)
 
-
-@auth.route('/profile')
-@login_required
-def profile():
-
-    return render_template('auth/update.html')
 
 @auth.route('/logout')
 @login_required
@@ -53,59 +47,101 @@ def logout():
     return redirect(url_for('main.index'))
 
 
-@auth.route('/addpost',methods=['POST','GET'])
+@auth.route('/addpost', methods=['POST', 'GET'])
 @login_required
 def addpost():
-    currentUser=current_user.username
-    addpost= PostForm()
+    currentUser = current_user.username
+    addpost = PostForm()
     if addpost.validate_on_submit():
-        post=Post(topic=addpost.topic.data,content=addpost.content.data,owner=currentUser)
-     
-        
-        post.save()
-       
-        return redirect(url_for('main.index'))
-    return render_template('auth/post.html',addpost=addpost,user=currentUser)
+        post = Post(topic=addpost.topic.data,
+                    content=addpost.content.data, owner=currentUser)
 
-@auth.route('/<postid>/comment',methods=['POST','GET'])
+        post.save()
+
+        return redirect(url_for('main.index'))
+    return render_template('auth/post.html', addpost=addpost, user=currentUser)
+
+
+@auth.route('/<postid>/comment', methods=['POST', 'GET'])
 @login_required
 def comment(postid):
-    
-    postitem=Post.query.filter_by(id=postid).first()
-    postowner=postitem.owner
-    user=current_user.username
-    items=Comment.query.filter_by(parentid=postid).all()
-    form=CommentForm()
-    if form.validate_on_submit():
-        post=Comment.query.filter_by(id=postid)
-        if post:
-            usercomment=form.comment.data
-           
-           
-            feedback= Comment(content=usercomment,owner=user)
-            feedback.save()
-            return redirect(url_for('main.index'))
-            
-    return render_template('auth/comment.html',form=form,item=items,postowner=postowner,user=user)
 
-    
-    
-@auth.route('/Subscribe',methods=['GET','POST'])
-def subsblog():
-    
+    postitem = Post.query.filter_by(id=postid).first()
+    postowner = postitem.owner
+    user = current_user.username
+    items = Comment.query.filter_by(parentid=postid).all()
+    form = CommentForm()
+    if form.validate_on_submit():
+        post = Comment.query.filter_by(id=postid)
+        if post:
+            usercomment = form.comment.data
+
+            feedback = Comment(content=usercomment,
+                               parentid=postitem.id, owner=user)
+            feedback.save()
+            send = Subscribe.query.filter_by(postid=postid).all()
+            for sub in send:
+                sendmail('At a Glance', 'email/notification',
+                         sub.email, user=current_user.username)
+            return redirect(url_for('main.index'))
+
+    return render_template('auth/comment.html', form=form, item=items, postowner=postowner, user=user, postitem=postitem)
+
+
+@auth.route('/subscribe/<postid>', methods=['GET', 'POST'])
+def subsblog(postid):
+
     form = SubscriberForm()
     if form.validate_on_submit():
-        subs = Subscribe(email = form.email.data, username = form.username.data)    
+        subs = Subscribe(email=form.email.data,
+                         username=form.username.data, postid=postid)
         db.session.add(subs)
         db.session.commit()
 
         return redirect(url_for('main.index'))
-    return render_template('auth/subscribe.html',subscribe_form=form)
+    return render_template('auth/subscribe.html', subscribe_form=form)
 
-@auth.route('/<commentid>/delete')
-def delete(commentid):
-    comment=Comment.query.filter_by(id=commentid).delete()
-    
-    
-    db.session.commit()
-    return redirect(url_for('auth.comment'))
+
+@auth.route('/<id>/deletecomment')
+def delete(id):
+    comment = Comment.query.filter_by(id=id).delete()
+    if comment:
+        db.session.commit()
+
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/profile')
+@login_required
+def profile():
+    title = 'User Profile'
+    uname = current_user.username
+    post = Post.query.filter_by(owner=uname).all()
+    return render_template('auth/profile.html', title=title, post=post)
+
+
+@auth.route('/<postid>/deletepost')
+@login_required
+def deletepost(postid):
+    post = Post.query.filter_by(id=postid).delete()
+    if post:
+        db.session.commit()
+        return redirect(url_for('auth.profile'))
+
+
+@auth.route('/<postid>/update',methods=['GET','POST'])
+@login_required
+def updatepost(postid):
+    form = PostUpdate()
+    titleupdate=TitleUpdate()
+    title="Post Update"
+    post = Post.query.filter_by(id=postid).first()
+    if post:
+        if form.validate_on_submit():
+            update = Post.query.filter_by(id=postid).update(
+                {"content": form.content.data})
+            update
+            db.session.commit()
+            return redirect(url_for('auth.profile'))
+
+    return render_template('auth/update.html', postform=form,title=title)
--- app/main/views.py
+++ app/main/views.py
@@ -4,13 +4,13 @@ from . import main
 from ..models import Comment,Post
 from flask_login import current_user
 from sqlalchemy import desc,select
-# from flask_login import current_user,login_required
+from flask_login import current_user,login_required
 
 @main.route('/')
 def index():
     
     title="home"
     quote=get_quote()
-    posts= Post.query.all()
+    posts= Post.query.order_by(Post.posted.desc())
     
     return render_template('index.html',title=title,quote=quote,posts=posts)
